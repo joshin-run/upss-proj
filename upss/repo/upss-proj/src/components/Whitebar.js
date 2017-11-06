07c3e9a67ab037b16f18bd148c6cf46ec3f4e39f
joshin-run <jahmsc@gmail.com>
2017-11-06 11:32:48 -0700

    Feat (style) trying to figure out how to style the space above the concern section

---
+++ upss/repo/upss-proj/src/components/Whitebar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Whitebar extends Component {
+  render() {
+    return (
+      <div className="whitebar">
+        <div className="ididntknow-text-container">
+          <p>I didn’t know...<br /></p>
+          <p><span>my “Cosmetic Surgeon” wasn’t a Plastic Surgeon.</span></p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Whitebar;
