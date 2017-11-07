6959d3fc49562edb892531f17813e62e6f9b1d99
joshin-run <jahmsc@gmail.com>
2017-11-07 13:19:30 -0700

    Feat (style) info section; all but margin issue on right side

--- upss/repo/upss-proj/src/containers/Info.js
+++ upss/repo/upss-proj/src/containers/Info.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import hexigonDoc from '../images/hexigonDoc.png'
 import upssLogo from '../images/logos/upss-logo-text.png'
+import toonDoc from '../images/toon-doc.svg'
 import aspsLogo from '../images/logos/ASPS_logo_300dpi.jpg'
+import steth from '../images/steth.png'
 
 class Info extends Component {
   render() {
@@ -26,14 +28,27 @@ class Info extends Component {
           UPSS Membership
         </p>
         <div className="upss-membership-container">
-
+          <div className="upss-membership-content">
+            <div className="upss-membership-text">
+              <p>In order to qualify for membership in the Utah Plastic Surgery Society, a surgeon must be properly trained and be certified by the <a href="https://www.abplasticsurgery.org/" target="_blank"><span className="link">American Board of Plastic Surgery.</span></a></p><br />
+              <p>Prospective patients can be assured that UPSS surgeons have successfully met the most rigorous academic and professional requirements, and have demonstrated outstanding surgical skill throughout their careers.</p>
+            </div>
+            <img className="toon-doctor-image"  src={toonDoc} alt="Cartoon Male Doctor" />
+          </div>
         </div>
         <p className="info-section-title">
          American Society of<br />
          Plastic Surgeons
         </p>
         <div className="asps-container">
-
+          <img className="asps-image" src={steth} />
+          <div className="asps-content">
+            <img className="asps-logo" src={aspsLogo} alt="ASPS Logo" />
+            <div className="">
+              <p>Only a board certified plastic surgeons will provide the medical expertise you deserve.</p><br />
+              <p>Learn more about the <a href="https://www.plasticsurgery.org/" target="_blank"><span className="link">American Society of Plastic Surgeons</span></a></p><br />
+            </div>
+          </div>
         </div>
 
       </div>
