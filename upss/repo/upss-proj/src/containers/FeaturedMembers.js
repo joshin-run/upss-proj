632d8ec52ddae78e5e6fec156368b568846a39f0
joshin-run <jahmsc@gmail.com>
2017-11-07 08:23:25 -0700

    Feat (style) member card

--- upss/repo/upss-proj/src/containers/FeaturedMembers.js
+++ upss/repo/upss-proj/src/containers/FeaturedMembers.js
@@ -1,13 +1,99 @@
 import React, { Component } from 'react';
+import exDocPic from '../images/doctors/ex_doc_pic_235x360.png'
 
 class FeaturedMembers extends Component {
   render() {
     return (
       <div className="featured-members-wrapper">
         <p className="page-header">Featured Members</p>
-        <p className="header-sub-title">All Society Members</p>
-        <p className="header-sub-title">are Board Certified By</p>
+        <p className="header-sub-title">All Society Members are</p>
+        <p className="header-sub-title">Board Certified by</p>
         <a href="https://www.plasticsurgery.org/"><p className="content">The American Board of Plastic Surgery</p></a>
+        <div className="member-card">
+          <img className="member-pic" href="" src={exDocPic} />
+          <div className="contact-info-container">
+            <p className="member-name">
+              Brian K. Brzowski, M.D.
+            </p>
+            <p className="member-address">
+              1525 East 6000 South,<br />
+              Suite C,<br />
+              South Ogden, UT 84405
+            </p>
+            <p className="member-phone-number">
+              (801) 479-5722
+            </p>
+            <p className="member-site">
+              www.brzowski.com
+            </p>
+          </div>
+        </div>
+        <div className="member-card">
+          <img className="member-pic" href="" src={exDocPic} />
+          <div className="contact-info-container">
+            <p className="member-name">
+              Brian K. Brzowski, M.D.
+            </p>
+            <p className="member-address">
+              1525 East 6000 South,<br />
+              Suite C,<br />
+              South Ogden, UT 84405
+            </p>
+            <p className="member-phone-number">
+              (801) 479-5722
+            </p>
+            <p className="member-site">
+              www.brzowski.com
+            </p>
+          </div>
+        </div>
+        <div className="member-card">
+          <img className="member-pic" href="" src={exDocPic} />
+          <div className="contact-info-container">
+            <p className="member-name">
+              Brian K. Brzowski, M.D.
+            </p>
+            <p className="member-address">
+              1525 East 6000 South,<br />
+              Suite C,<br />
+              South Ogden, UT 84405
+            </p>
+            <p className="member-phone-number">
+              (801) 479-5722
+            </p>
+            <p className="member-site">
+              www.brzowski.com
+            </p>
+          </div>
+        </div>
+        <div className="member-card">
+          <img className="member-pic" href="" src={exDocPic} />
+          <div className="contact-info-container">
+            <p className="member-name">
+              Brian K. Brzowski, M.D.
+            </p>
+            <p className="member-address">
+              1525 East 6000 South,<br />
+              Suite C,<br />
+              South Ogden, UT 84405
+            </p>
+            <p className="member-phone-number">
+              (801) 479-5722
+            </p>
+            <p className="member-site">
+              www.brzowski.com
+            </p>
+          </div>
+        </div>
+        <div className="member-card">
+
+        </div>
+        <div className="member-card">
+
+        </div>
+        <div className="member-card">
+
+        </div>
       </div>
     );
   }
